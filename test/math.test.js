dc6149c6ec9dc6d8e719a786c60bd7b01bf9216a
Ethan <thrillerskillz29@gmail.com>
2026-07-27 14:48:58 +1200

    Add missing test coverage for subtract()

--- test/math.test.js
+++ test/math.test.js
@@ -1,7 +1,10 @@
 const assert = require('node:assert');
-const { add } = require('../src/math');
+const { add, subtract } = require('../src/math');
 
 assert.strictEqual(add(2, 3), 5);
 assert.strictEqual(add(-1, 1), 0);
+
+assert.strictEqual(subtract(5, 3), 2);
+assert.strictEqual(subtract(0, 5), -5);
 
 console.log('All tests passed.');
